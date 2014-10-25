bf2b58c305c99ca9a80f80d1a1024cd0113e1abc
Mikhail pudov <strelocpm@gmail.com>
2014-10-25 05:22:50 +0400

    Start

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПраграммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПраграммеToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // оПраграммеToolStripMenuItem
+            // 
+            this.оПраграммеToolStripMenuItem.Name = "оПраграммеToolStripMenuItem";
+            this.оПраграммеToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.оПраграммеToolStripMenuItem.Text = "О праграмме";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(680, 367);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(686, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(455, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 472);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПраграммеToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+
+
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+   
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public struct adjacency_matrix
+        {
+            public int Nambers;
+            public char Name;
+            public int x, y;
+        }
+        public class Translate
+        {
+            int i = 0,j=0; 
+          public  char[][] Matrix = new char[3][];
+          public char[][] TMatrix = new char[3][];
+           // char[,] AMatrix = new char[,];
+           // int[,] MatrixXY = new int[k, 2];
+            adjacency_matrix[] AMarix = new adjacency_matrix[3*5];
+            
+
+
+
+           public void StringToInt(string[] s)
+            {
+                
+                for (; j < s.Length; j++)
+                {
+                    Matrix[i][j] = Convert.ToChar(s[j]);
+                }
+                    i++;
+                    j = 0;
+            }
+           public void zap()
+           {
+                Matrix[0] = new char[] { 'a', 'd', 's', 'm', 't' };
+                Matrix[1] = new char[] { 'n', 't', 'l', 'u', 'o' };
+                Matrix[2] = new char[] { 's', 'm', 'q', 't', 'p' };
+                TMatrix = Matrix;
+               // string[] y;                                                                                                                               
+                 //y[0] =   Convert.ToString(Matrix[0]);
+               // listBox1.Items.Add(Matrix[0][1]);
+           }
+            public bool Razbor3(char a,int k)
+           {
+               for (int t = 0; t<=k;t++ ) {
+                   if (AMarix[t].Name == a) { return false; }
+               }
+               return true;
+           }
+           public void Razbor2(int t1, int t2)
+           {
+               int i2=0,j2=0;
+               bool trig=false;
+               for (int i1 = 0; i1 < Matrix.Length; i1++)
+               {
+                   for (int j1 = 0; j1 < Matrix[i1].Length; j1++)
+                   {
+                       if (Matrix[t1][t2] == Matrix[i1][j1] && (t1 != i1 && t2 != j1))
+                       {
+                           AMarix[i2].Nambers++;
+                       }
+                       
+
+                   }
+                   if (Razbor3(TMatrix[t1][t2], i2))
+                   {
+                       i2++;
+                   }
+                       AMarix[i2].Name = Matrix[t1][t2];
+                   
+                   
+                   
+                   
+               }
+                       
+           }
+            public void Razbor()
+           {
+               for (int i = 0; i < Matrix.Length; i++)
+               {
+                   for (int j = 0; j < Matrix[i].Length; j++)
+                   {
+                       Razbor2(i,j);
+                   }
+               }
+                  
+
+           }
+        }
+        public class Grafic
+        {
+            public Translate nex =new Translate();
+            public void Start()
+            {
+                nex.zap();
+                nex.Razbor();
+            }
+                 
+
+        }
+        public class Graf
+        {
+
+
+
+        }
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Translate t = new Translate();
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Вершины графа (*.txt)|*.txt|All files (*.*)|*.*";
+            
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            StreamReader file = new StreamReader(openFileDialog1.FileName);
+                            while (!file.EndOfStream) 
+                            {
+                                t.StringToInt(file.ReadLine().Split(' '));
+                            }
+                            file.Close();
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public double Radians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+        public int Ze(int n)
+        {
+            return Convert.ToInt32(360/n);
+        }
+        public bool Ysl()
+        {
+
+            return true;
+        }
+        public void relations()
+        {
+           
+
+            return;
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int k = 5;
+            int i = 0;
+            int r = 70;// *Convert.ToInt32(k / 50);
+            int sh = 70;
+            int k1 = Ze(k);
+            int[,] MatrixXY = new int[k,2];
+            Graphics g = e.Graphics;
+            String drawString = "G";
+
+            // Create font and brush.
+            Font drawFont = new Font("Arial", 14);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            for (int j = 0; j < k; j++)
+            {
+             
+                 MatrixXY[j,0] = sh + Convert.ToInt32(r * Math.Cos(Radians(i)));
+                 MatrixXY[j,1] = sh + Convert.ToInt32(r * Math.Sin(Radians(i)));
+                 e.Graphics.DrawEllipse(Pens.Black, MatrixXY[j, 0], MatrixXY[j, 1], 20, 20);
+                 e.Graphics.DrawString(drawString, drawFont, drawBrush, MatrixXY[j, 0]+3, MatrixXY[j, 1]-2);
+                 i += k1;
+            }
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Grafic r = new Grafic();
+            r.Start();
+           // listBox1.Items.Add(r.nex.Matrix[0].ToString());
+        }
+    }
+}
